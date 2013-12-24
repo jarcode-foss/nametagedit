bcb4c4ab4d5887d7f6d3ba4a9dad1a902cf475b1
JustOneMoreBlock <cgillenkirk@gmail.com>
2013-12-24 13:54:33 -0500

    Update Packet209Mod.java
    Fixes 1.7 compatibility, Provided By: sgtcaze

--- src/ca/wacos/nametagedit/Packet209Mod.java
+++ src/ca/wacos/nametagedit/Packet209Mod.java
@@ -1,3 +1,6 @@
+/**
+ * From @sgtcaze https://raw.github.com/sgtcaze/NametagEdit/master/src/main/java/ca/wacos/nametagedit/Packet209Mod.java
+ */
 package ca.wacos.nametagedit;
 
 import java.lang.reflect.InvocationTargetException;
@@ -14,95 +17,124 @@ import java.lang.reflect.Field;
  */
 class Packet209Mod {
 
-    Object packet;
+	Object packet;
 
-    private static Method getHandle;
-    private static Method sendPacket;
-    private static Field playerConnection;
+	private static Method getHandle;
+	private static Method sendPacket;
+	private static Field playerConnection;
 
-    private static Class<?> packetType;
+	private static Class<?> packetType;
 
-    static {
-        try {
+	static {
+		try {
 
-            packetType = Class.forName(getPacketTeamClasspath());
+			packetType = Class.forName(getPacketTeamClasspath());
 
-            Class<?> typeCraftPlayer = Class.forName(getCraftPlayerClasspath());
-            Class<?> typeNMSPlayer = Class.forName(getNMSPlayerClasspath());
-            Class<?> typePlayerConnection = Class.forName(getPlayerConnectionClasspath());
+			Class<?> typeCraftPlayer = Class.forName(getCraftPlayerClasspath());
+			Class<?> typeNMSPlayer = Class.forName(getNMSPlayerClasspath());
+			Class<?> typePlayerConnection = Class
+					.forName(getPlayerConnectionClasspath());
 
-            getHandle = typeCraftPlayer.getMethod("getHandle");
-            playerConnection = typeNMSPlayer.getField("playerConnection");
-            sendPacket = typePlayerConnection.getMethod("sendPacket", Class.forName(getPacketClasspath()));
-        }
-        catch (Exception e) {
-            System.out.println("Failed to setup reflection for Packet209Mod!");
-            e.printStackTrace();
-        }
-    }
+			getHandle = typeCraftPlayer.getMethod("getHandle");
+			playerConnection = typeNMSPlayer.getField("playerConnection");
+			sendPacket = typePlayerConnection.getMethod("sendPacket",
+					Class.forName(getPacketClasspath()));
+		} catch (Exception e) {
+			System.out.println("Failed to setup reflection for Packet209Mod!");
+			e.printStackTrace();
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    Packet209Mod(String name, String prefix, String suffix, Collection players, int paramInt)
-            throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, NoSuchFieldException, InvocationTargetException {
+	@SuppressWarnings("rawtypes")
+	Packet209Mod(String name, String prefix, String suffix, Collection players,
+			int paramInt) throws ClassNotFoundException,
+			IllegalAccessException, InstantiationException,
+			NoSuchMethodException, NoSuchFieldException,
+			InvocationTargetException {
 
-        packet = packetType.newInstance();
+		packet = packetType.newInstance();
+		setField("a", name);
+		setField("f", paramInt);
 
-        setField("a", name);
-        setField("f", paramInt);
+		if ((paramInt == 0) || (paramInt == 2)) {
+			setField("b", name);
+			setField("c", prefix);
+			setField("d", suffix);
+			setField("g", 1);
+		}
+		if (paramInt == 0)
+			addAll(players);
+	}
 
-        if ((paramInt == 0) || (paramInt == 2)) {
-            setField("b", name);
-            setField("c", prefix);
-            setField("d", suffix);
-            setField("g", 1);
-        }
-        if (paramInt == 0)
-            addAll(players);
-    }
-    @SuppressWarnings("unchecked")
-    Packet209Mod(String name, Collection players, int paramInt)
-            throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, NoSuchFieldException, InvocationTargetException {
+	@SuppressWarnings("rawtypes")
+	Packet209Mod(String name, Collection players, int paramInt)
+			throws ClassNotFoundException, IllegalAccessException,
+			InstantiationException, NoSuchMethodException,
+			NoSuchFieldException, InvocationTargetException {
 
-        packet = packetType.newInstance();
+		packet = packetType.newInstance();
 
-        if ((paramInt != 3) && (paramInt != 4)) throw new IllegalArgumentException("Method must be join or leave for player constructor");
-        if ((players == null) || (players.isEmpty())) players = new ArrayList<String>();
+		if ((paramInt != 3) && (paramInt != 4))
+			throw new IllegalArgumentException(
+					"Method must be join or leave for player constructor");
+		if ((players == null) || (players.isEmpty()))
+			players = new ArrayList<String>();
 
-        setField("a", name);
-        setField("f", paramInt);
-        addAll(players);
-    }
-    void sendToPlayer(Player bukkitPlayer)
-            throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException, NoSuchFieldException {
+		setField("a", name);
+		setField("f", paramInt);
+		addAll(players);
+	}
 
-        Object player = getHandle.invoke(bukkitPlayer);
+	void sendToPlayer(Player bukkitPlayer) throws ClassNotFoundException,
+			IllegalAccessException, InstantiationException,
+			InvocationTargetException, NoSuchMethodException,
+			NoSuchFieldException {
 
-        Object connection = playerConnection.get(player);
+		Object player = getHandle.invoke(bukkitPlayer);
 
-        sendPacket.invoke(connection, packet);
-    }
-    private void setField(String field, Object value)
-            throws NoSuchFieldException, IllegalAccessException {
-        packet.getClass().getField(field).set(packet, value);
-    }
-    @SuppressWarnings("unchecked")
-    private void addAll(Collection<?> col)
-            throws NoSuchFieldException, IllegalAccessException {
-        ((Collection) packet.getClass().getField("e").get(packet)).addAll(col);
-    }
-    private static String getCraftPlayerClasspath() {
-        return "org.bukkit.craftbukkit." + PackageChecker.getVersion() + ".entity.CraftPlayer";
-    }
-    private static String getPlayerConnectionClasspath() {
-        return "net.minecraft.server." + PackageChecker.getVersion() + ".PlayerConnection";
-    }
-    private static String getNMSPlayerClasspath() {
-        return "net.minecraft.server." + PackageChecker.getVersion() + ".EntityPlayer";
-    }
-    private static String getPacketClasspath() {
-        return "net.minecraft.server." + PackageChecker.getVersion() + ".Packet";
-    }
-    private static String getPacketTeamClasspath() {
-        return "net.minecraft.server." + PackageChecker.getVersion() + ".Packet209SetScoreboardTeam";
-    }
+		Object connection = playerConnection.get(player);
+
+		sendPacket.invoke(connection, packet);
+	}
+
+	private void setField(String field, Object value)
+			throws NoSuchFieldException, IllegalAccessException {
+		Field f = packet.getClass().getDeclaredField(field);
+		f.setAccessible(true);
+		f.set(packet, value);
+
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void addAll(Collection<?> col) throws NoSuchFieldException,
+			IllegalAccessException {
+		Field f = packet.getClass().getDeclaredField("e");
+		f.setAccessible(true);
+		((Collection) f.get(packet)).addAll(col);
+	}
+
+	private static String getCraftPlayerClasspath() {
+		return "org.bukkit.craftbukkit." + PackageChecker.getVersion()
+				+ ".entity.CraftPlayer";
+	}
+
+	private static String getPlayerConnectionClasspath() {
+		return "net.minecraft.server." + PackageChecker.getVersion()
+				+ ".PlayerConnection";
+	}
+
+	private static String getNMSPlayerClasspath() {
+		return "net.minecraft.server." + PackageChecker.getVersion()
+				+ ".EntityPlayer";
+	}
+
+	private static String getPacketClasspath() {
+		return "net.minecraft.server." + PackageChecker.getVersion()
+				+ ".Packet";
+	}
+
+	private static String getPacketTeamClasspath() {
+		return "net.minecraft.server." + PackageChecker.getVersion()
+				+ ".PacketPlayOutScoreboardTeam";
+	}
 }
